d85716878b7272712e324bce355263c4ae2bb8f9
Jussi Haaja <jussi.haaja@ambientia.fi>
2018-10-31 17:36:07 +0200

    Project management told it needs more gif

--- src/main/java/net/ambientia/osaw/demo/DemoController.java
+++ src/main/java/net/ambientia/osaw/demo/DemoController.java
@@ -8,6 +8,7 @@ public class DemoController {
 
 @RequestMapping("/")
   public String index() {
-    return "<html><head><title>OSAW demo</title></head><body><h1>Good morning Kista!</h1></body></html>";
+    //return "<html><head><title>OSAW demo</title></head><body><h1>Good morning Kista!</h1></body></html>";
+    return "<html><head><title>OSAW demo</title></head><body><img src=\"http://jussiha.staff.ambientia.fi/images/cat.gif\"></img></body></html>";
   }
 }
