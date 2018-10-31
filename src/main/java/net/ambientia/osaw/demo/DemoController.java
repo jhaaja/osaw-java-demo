9162772c3ec681e0ceaf1dbc49538d267f7da9a3
Jussi Haaja <jussi.haaja@ambientia.fi>
2018-10-31 19:23:26 +0200

    New release

--- src/main/java/net/ambientia/osaw/demo/DemoController.java
+++ src/main/java/net/ambientia/osaw/demo/DemoController.java
@@ -9,6 +9,6 @@ public class DemoController {
 @RequestMapping("/")
   public String index() {
     //return "<html><head><title>OSAW demo</title></head><body><h1>Good morning Kista!</h1></body></html>";
-    return "<html><head><title>OSAW demo</title></head><body><img src=\"http://jussiha.staff.ambientia.fi/images/cat.gif\"></img></body></html>";
+    return "<html><head><title>Car Configurator</title></head><body><h1>Welcome to Car Configurator!</h1><p>Please see our latest model:<br/><img src=\"http://mirror.raatti.net/v70-malmo.jpg\"></img></p></body></html>";
   }
 }
