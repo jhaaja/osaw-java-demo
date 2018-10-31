ee56e7cca9039a10d97876bacd634b5aa3e10137
Jussi Haaja <jussi.haaja@ambientia.fi>
2018-10-31 16:57:36 +0200

    Initial demo application

---
+++ src/main/java/net/ambientia/osaw/demo/DemoController.java
@@ -0,0 +1,13 @@
+package net.ambientia.osaw.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class DemoController {
+
+@RequestMapping("/")
+  public String index() {
+    return "Good morning Kista!";
+  }
+}
